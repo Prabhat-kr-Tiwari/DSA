651bc01ef1eef39a66606749282cdcd60f33f85a
Prabhat Kumar Tiwari <Sonukbc6@gmail.com>
2022-06-12 14:13:03 +0530

    DSA

--- src/Array2/leaderinarray.java
+++ src/Array2/leaderinarray.java
@@ -1,7 +1,7 @@
 package Array2;
 
 
-
+import java.util.ArrayList;
 
 public class leaderinarray {
     static void leaderInArrayUsingLoops(int a[])
@@ -17,14 +17,54 @@ public class leaderinarray {
             }
             if (isLeader)
             {
-                System.out.println(a[i]);
+                System.out.println(a[i]+" ");
             }
 
         }
     }
+    //optimize
+    static void isleader(int a[])
+    {
+        int largest=Integer.MIN_VALUE;
+        for (int i = a.length-1; i >=0 ; i--) {
+            if (a[i]>largest)
+            {
+                largest=a[i];
+                System.out.println(a[i]+" ");
+            }
 
+        }
+    }
+    static ArrayList<Integer> leaders(int arr[], int n){
+        ArrayList<Integer> al=new ArrayList<>();
+        ArrayList<Integer> all=new ArrayList<>();
+        //int largest=Integer.MIN_VALUE;
+        int largest=arr[n-1];
+        al.add(largest);
+        for(int i=arr.length-2;i>=0;i--)
+        {
+            if(arr[i]>=largest)
+            {
+                largest=arr[i];
+                al.add(largest);
+            }
+        }
+        for(int k=al.size()-1;k>=0;k--)
+        {
+            all.add(al.get(k));
+        }
+        return all;
+    }
     public static void main(String[] args) {
         int a[]={2,7,6,1,4,3};
+        int n=a.length;
+        System.out.println("Using loop");
         leaderInArrayUsingLoops(a);
+        System.out.println("using optimize o(N)");
+        isleader(a);
+        System.out.println("using arraylist");
+        System.out.println(leaders(a,n));
+
+
     }
 }
---
+++ src/Array2/maximumsumsubarray.java
@@ -0,0 +1,42 @@
+package Array2;
+
+public class maximumsumsubarray {
+    /*static int maxsum(int a[])
+    {
+        int max=Integer.MIN_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+            int sum=0;
+            for (int j = i; j < a.length; j++) {
+                sum=a[i]+a[j];
+                if (sum>max)
+                {
+                    max=sum;
+                }
+            }
+        }
+        return max;
+    }*/
+    static int largestmaximumsumsubarray(int a[])
+    {
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            currsum+=a[i];
+            if (maxsum<currsum)
+            {
+                maxsum=currsum;
+            }
+            if (currsum<0)
+            {
+                currsum=0;
+            }
+        }
+        return  maxsum;
+    }
+    public static void main(String[] args) {
+        int a[]={6,-7,4,-2,1,5,-4};
+        //System.out.println(maxsum(a));
+        System.out.println(largestmaximumsumsubarray(a));
+    }
+}
