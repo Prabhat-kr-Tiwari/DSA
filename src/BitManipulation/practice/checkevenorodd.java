1a32b7a87b3f0421b929be6b7b5fabd79a15f081
Prabhat Kumar Tiwari <Sonukbc6@gmail.com>
2022-07-03 17:35:57 +0530

    DSA

--- src/BitManipulation/practice/checkevenorodd.java
+++ src/BitManipulation/practice/checkevenorodd.java
@@ -12,12 +12,24 @@ public class checkevenorodd {
             System.out.println("odd");
         }
     }
+    //using and operation
+    static void Checkevenoroddusingandoperation(int n)
+    {
+        if ((n&1)==0)
+        {
+            System.out.println("even" );
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
 
 
 
 
     public static void main(String[] args) {
         Checkevenorodd(244);
+        Checkevenoroddusingandoperation(244);
 
     }
 }
