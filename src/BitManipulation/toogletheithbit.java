a85767c7f17a859dd8bf2e6b59dd5d531b35428e
Prabhat Kumar Tiwari <Sonukbc6@gmail.com>
2022-07-03 15:05:13 +0530

    DSA

---
+++ src/BitManipulation/toogletheithbit.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class toogletheithbit {
+    static int Toogletheithbit(int n,int i)
+    {
+        int mask=1<<i;
+        int res=n^mask;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Toogletheithbit(5,1));
+    }
+}
