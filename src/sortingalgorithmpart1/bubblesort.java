38372a093a0f7536c8809e5deadb8fb8747977e7
Prabhat Kumar Tiwari <Sonukbc6@gmail.com>
2022-07-10 11:16:56 +0530

    DSA

---
+++ src/sortingalgorithmpart1/bubblesort.java
@@ -0,0 +1,36 @@
+package sortingalgorithmpart1;
+
+public class bubblesort {
+    static void swap(int a[],int i,int j)
+    {
+        int temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+
+    }
+
+    static void BubbleSort(int a[])
+    {
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j < a.length-i-1; j++) {
+                if (a[j]>a[j+1])
+                {
+                    swap(a,j,j+1);
+                }
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[]={2,16,3,67,0};
+      BubbleSort(a);
+        for (int e:a
+             ) {
+            System.out.println(e);
+
+        }
+
+    }
+}
