95abe17b5d177c03c12ae57ea7a17bb72d029335
Prabhat Kumar Tiwari <Sonukbc6@gmail.com>
2022-07-26 19:52:04 +0530

    DSA

--- src/JavaCollectionFramework/set.java
+++ src/JavaCollectionFramework/set.java
@@ -1,15 +1,37 @@
 package JavaCollectionFramework;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class set {
     public static void main(String[] args) {
         Set<String>set=new HashSet<>();
         set.add("Apple");
         set.add("orange");
-
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Kiwi");
         System.out.println(set);
+        TreeSet<String>treeSet=new TreeSet<>();//treeset sort the element
+        treeSet.addAll(set);
+        System.out.println(treeSet);
+        LinkedHashSet<String> linkedHashSet=new  LinkedHashSet<>();//it maintains the order
+        linkedHashSet.addAll(set);
+        System.out.println(linkedHashSet);
+
+
+        Set<String>set1=new HashSet<>();
+        set1.add("Apple");
+        set1.add("orange");
+
+        set1.add("Kiwi");
+        set.removeAll(set1);
+        System.out.println("set1"+set1);
+        System.out.println("set"+set);
+
+
     }
 
 }
